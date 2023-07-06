d87a2bf8de086abb1523edd6f5419f79b215b76b
Aleksey Yefremenko <alexeyefr@gmail.com>
2023-07-06 22:18:02 +0300

    Add: register route

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
 use App\Http\Resources\UserResource;
 use App\Interfaces\UserRepositoryInterface;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -16,11 +20,13 @@ class LoginController extends Controller
         $this->userRepository = $userRepository;
     }
 
-    public function login(LoginRequest $request)
+    public function login(LoginRequest $request): JsonResponse
     {
         $credentials = $request->only(['email', 'password']);
 
         if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
             return response()->json([
                 'status' => 'ok',
                 'message' => 'Login successful',
@@ -32,8 +38,33 @@ class LoginController extends Controller
             'status' => 'error',
             'message' => 'This credentials are not valid!',
             'data' => [],
-        ]);
+        ], Response::HTTP_UNAUTHORIZED);
     }
 
+    public function register(RegisterRequest $request): JsonResponse
+    {
+        $userData = $request->only([
+            'name',
+            'email',
+        ]);
+        $userData['password'] = Hash::make($request->get('password'));
 
+        $user = $this->userRepository->createUser($userData);
+
+        if ($user) {
+            Auth::login($user);
+
+            return response()->json([
+                'status' => 'ok',
+                'message' => 'Registration successful',
+                'data' => UserResource::make($user),
+            ]);
+        }
+
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Error when creating new user',
+            'data' => [],
+        ], Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::post('login', [LoginController::class, 'login']);
+Route::post('register', [LoginController::class, 'register']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
